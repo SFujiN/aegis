45cbc6cc9f704b8c7b2a112dd183ce737be073da
golem019 <golem019@umn.edu>
2021-03-07 19:11:02 -0600

    added Candidate functionality

---
+++ Project1/src/Candidate.cpp
@@ -0,0 +1,20 @@
+#include "Candidate.h"
+
+std::string Candidate::getName(){
+  return name;
+}
+char Candidate::getParty(){
+  return party;
+}
+int Candidate::getNumVotes(){
+  return numVotes;
+}
+void Candidate::updateVote(int newVotes){
+  numVotes = newVotes;
+}
+void Candidate::incrVotes(){
+  numVotes++;
+}
+void Candidate::addBallot(Ballot newBallot){
+  ballots.push_back(newBallot);
+}
