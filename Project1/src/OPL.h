3cc305048663fec60a1efb589a4bdb82736b09f6
golem019 <golem019@umn.edu>
2021-03-08 23:40:34 -0600

    added some functions and partial functionality for runElection

--- Project1/src/OPL.h
+++ Project1/src/OPL.h
@@ -1,18 +1,30 @@
 #ifndef OPL_H
 #define OPL_H
+//TODO: delete these and put in votingSystem
+#include <algorithm>
 #include <vector>
+#include <fstream>
+#include <sstream>
+
+#include "Party.h"
 
 class OPL : public VotingSystem {
-  public:
-    void groupIndependentParties();
-    void allocateSeats(Party);
-    void findPartyWinners();
-    int findQuota();
+ public:
+  void groupIndependentParties();
+  void allocateSeats();
+  void findPartyWinners();
+  bool sortRemainder(Party first, Party second) {
+    return first.getRemainder() < second.getRemainder();
+  }
+  bool sortNumBallots(Party first, Party second) {
+    return first.getNumBallots() < second.getNumBallots();
+  }
 
-  private:
-    int numSeats;
-    int quota;
-    std::vector<Party> parties;
+ private:
+  int numSeats;
+  int quota;
+  std::vector<Party> parties;
+  int numCandidates;
 };
 
 #endif OPL_H
