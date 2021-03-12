41080af88935e707a09db64348698e7a998636fd
vang2959 <vang2959@umn.edu>
2021-03-12 12:02:50 -0600

    adding BallotTest

--- Project1/testing/BallotTest.cpp
+++ Project1/testing/BallotTest.cpp
@@ -1,7 +1,5 @@
-#include "gtest/gtest.h"
-#include "Ballot.h"
-#include"Candidate.h"
-#include <vector>
+#include "googletest/googletest/include/gtest/gtest.h"
+#include "../src/Ballot.h"
 
 
 class BallotTest : public ::testing::Test {
@@ -12,7 +10,7 @@ public:
         Jane = new Candidate(“D”,7,”Jane”);
         David = new Candidate(“L”,1000,”Dave”);
 
-        ballot1={Bob,John,Jane,David};
+        ballot1={Bob,Jane,David};
     }
 
 protected:
@@ -24,13 +22,21 @@ protected:
 };
 
 TEST_F (BallotTest, incrCurrent) {
+    EXPECT_EQ(Bob, b1.getCandidate());
     b1.incrCurrent(); //currBallot = 1
-    EXPECT_EQ(John, b1.getCandidate());
+    EXPECT_EQ(Jane, b1.getCandidate());
+}
+
+TEST_F (BallotTest, getCandidate) { 
+    EXPECT_EQ(Bob, b1.getCandidate());
+    b1.incrCurrent(); //currBallot = 1
     b1.incrCurrent(); //currBallot = 2
     b1.incrCurrent(); //currBallot = 3
     EXPECT_EQ(nullptr, b1.getCandidate());
 }
 
-TEST_F (BallotTest, getCandidate) { 
-    EXPECT_EQ(Bob, b1.getCandidate());
+int main(int argc, char ∗∗argv) {
+    printf("Please work");
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
