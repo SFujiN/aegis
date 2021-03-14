678f6c7c5ca994b8fc9a49d35d3ee359d2c6ca61
golem019 <golem019@umn.edu>
2021-03-13 22:00:08 -0600

    doxy for ballot.h

--- Project1/src/Ballot.h
+++ Project1/src/Ballot.h
@@ -1,22 +1,57 @@
+/**
+ * @file Ballot.h
+ *
+ * @copyright 2020, All rights reserved.
+ */
+
 #ifndef BALLOT_H
 #define BALLOT_H
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+
 #include <vector>
 
+
+  /*******************************************************************************
+   * Class Definitions
+   ******************************************************************************/
+
+  /**
+   * @brief A class representing Ballots, who will be assigned to Candidates based on their top vote.
+   */
 class Ballot {
  public:
-  // constructor
+  /**
+   * @brief Default constructor. Takes in only a vector of ints representing ranked choices and assigns it to candidates.
+   * 
+   * @param vector, vector of ints representing ranked chocies. Assigned to candidates.
+   */
   Ballot(std::vector<int> v1);
 
-  // setters methods
+  /**
+   * @brief Increments currBallot pointer to next value in the candidates vector.
+   */
   void incrCurrent();
 
-  // getters methods
+  /**
+   * @brief Method to return candidates vector.
+   * 
+   * @return Returns a vector of ints.
+   */
   std::vector<int> getCandidates();
+
+  /**
+   * @brief Method to get the current ballot index required to identify the candidate the ballot currently belongs to.
+   * 
+   * @return Returns an integer.
+   */
   int getCurrBallotIndex(); //candidates[ballot1.getCandidates().at(ballot1.getCurrent())-1]
 
  private:
-  std::vector<int> candidates;
-  int currBallot = 0;
+  std::vector<int> candidates; ///< A vector of ints representing the order in which the ballot favors candidates.
+  int currBallot = 0; ///< An integer representing which candidate the ballot is currently voting for. Starts at 0 and only increments by 1.
 };
 
 #endif
