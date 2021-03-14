be2128c27a2c3beb399e9b0fb48a7438ea5fa18b
golem019 <golem019@umn.edu>
2021-03-13 22:00:58 -0600

    reformatted to be cpplint compliant

--- Project1/src/Ballot.h
+++ Project1/src/Ballot.h
@@ -13,45 +13,52 @@
 
 #include <vector>
 
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
 
-  /*******************************************************************************
-   * Class Definitions
-   ******************************************************************************/
-
-  /**
-   * @brief A class representing Ballots, who will be assigned to Candidates based on their top vote.
-   */
+/**
+ * @brief A class representing Ballots, who will be assigned to Candidates based
+ * on their top vote.
+ */
 class Ballot {
  public:
   /**
-   * @brief Default constructor. Takes in only a vector of ints representing ranked choices and assigns it to candidates.
-   * 
-   * @param vector, vector of ints representing ranked chocies. Assigned to candidates.
+   * @brief Default constructor. Takes in only a vector of ints representing
+   * ranked choices and assigns it to candidates.
+   *
+   * @param vector, vector of ints representing ranked chocies. Assigned to
+   * candidates.
    */
   Ballot(std::vector<int> v1);
 
   /**
-   * @brief Increments currBallot pointer to next value in the candidates vector.
+   * @brief Increments currBallot pointer to next value in the candidates
+   * vector.
    */
   void incrCurrent();
 
   /**
    * @brief Method to return candidates vector.
-   * 
+   *
    * @return Returns a vector of ints.
    */
   std::vector<int> getCandidates();
 
   /**
-   * @brief Method to get the current ballot index required to identify the candidate the ballot currently belongs to.
-   * 
+   * @brief Method to get the current ballot index required to identify the
+   * candidate the ballot currently belongs to.
+   *
    * @return Returns an integer.
    */
-  int getCurrBallotIndex(); //candidates[ballot1.getCandidates().at(ballot1.getCurrent())-1]
+  int getCurrBallotIndex();  // candidates[ballot1.getCandidates().at(ballot1.getCurrent())-1]
 
  private:
-  std::vector<int> candidates; ///< A vector of ints representing the order in which the ballot favors candidates.
-  int currBallot = 0; ///< An integer representing which candidate the ballot is currently voting for. Starts at 0 and only increments by 1.
+  std::vector<int> candidates;  ///< A vector of ints representing the order in
+                                ///< which the ballot favors candidates.
+  int currBallot =
+      0;  ///< An integer representing which candidate the ballot is currently
+          ///< voting for. Starts at 0 and only increments by 1.
 };
 
 #endif
