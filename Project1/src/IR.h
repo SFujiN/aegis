d3abde0bce2b4fef6c65f6efbd749f6b312cde2e
Michael <ly000017@umn.edu>
2021-03-14 00:50:57 -0600

    Adding doxy comments to OPL and IR

--- Project1/src/IR.h
+++ Project1/src/IR.h
@@ -1,6 +1,16 @@
+/**
+ * @file IR.h
+ *
+ * @copyright 2021 Aegis, All rights reserved.
+ */
+
+
+
 #ifndef IR_H
 #define IR_H
-
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
 #include <algorithm>
 #include <fstream>
 #include <sstream>
@@ -13,12 +23,45 @@
 class IR : public VotingSystem{
 
 public:
+
+/**
+ * @brief Empty constructor for the IR election.
+ * 
+ 
+
+*/
 IR();
 
 Candidate findWinner();
+/**
+ * @brief Attempts to find a winner in the IR election.
+ * 
+ *
+ * 
+ * @return Candidate.
+
+*/
 void elimination();
 
+/**
+ * @brief Eliminates a candidate.
+ * 
+ * Recieves the candidate with the lowest ballots and redistributes their ballots
+ * 
+ * @return void.
+
+*/
+
 Candidate breakTie(Candidate a, Candidate b);
+
+/**
+ * @brief Returns a candidate after tie.
+ * 
+ * Takes in two candidates that are "tied" and randomly decides one to remove.
+ * 
+ * @return char.
+
+*/
 
 private:
 
