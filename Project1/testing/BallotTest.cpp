0b6f36d4398d682c9bfc14e5fab681f61a9534bb
Michael <ly000017@umn.edu>
2021-03-15 16:02:36 -0500

    Adding base tests to release

--- Project1/testing/BallotTest.cpp
+++ Project1/testing/BallotTest.cpp
@@ -1,45 +1,79 @@
-#include "../src/Candidate.h"
-#include "gtest/gtest.h"
+#include <vector>
 
-class BallotTest : public ::testing::Test {
- public:
-    BallotTest();
-  void SetUp() {
-    // ballot1 is: 3, 1, 2
-    // ballot1 new order is: 2, 3, 1
-    Bob = Candidate('I', "Bob");
-    candidates.push_back(Bob);
-    Jane = Candidate('D', "Jane");
-    candidates.push_back(Jane);
-    David = Candidate('L', "Dave");
-    candidates.push_back(David);
-    ballotCandidates.push_back(2);
-    ballotCandidates.push_back(3);
-    ballotCandidates.push_back(1);
-    Ballot ballot1(ballotCandidates);
-  }
+#include "../src/Ballot.cpp"
+#include "../src/Candidate.cpp"
+#include "../src/OPL.cpp"
+#include "../src/Party.cpp"
+#include "../src/VotingSystem.cpp"
 
-  void TearDown() {}
-  // Candidate Norm;
-  Candidate Bob;
-  Candidate Jane;
-  Candidate David;
-  Ballot ballot1;
-  std::vector<int> ballotCandidates;
-  std::vector<Candidate> candidates;
-};
+using namespace std;
+    Candidate Bob = Candidate('I', "Bob");
+    
+    Candidate Jane = Candidate('D', "Jane");
+    
+   Candidate David = Candidate('L', "Dave");
+    
+    
 
-TEST_F(BallotTest, incrCurrent) {
-  // int current = ballot1.getCurrent();
-  // int cand = ballot1.getCandidates().at(current);
-  // ballot1.getCandidates().at(ballot1.getCurrent());
-  EXPECT_EQ(Jane, candidates[ballot1.getCurrBallotIndex()]);
-  ballot1.incrCurrent();  // currBallot = 1
-  EXPECT_EQ(David, candidates[ballot1.getCurrBallotIndex()]);
-}
 
 int main(int argc, char *argv[]) {
-  printf("Please work");
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  std::vector<int> ballotCandidates;
+  std::vector<Candidate> candidates;
+  candidates.push_back(Bob);
+  candidates.push_back(Jane);
+  candidates.push_back(David);
+  ballotCandidates.push_back(2);
+  ballotCandidates.push_back(3);
+  ballotCandidates.push_back(1);
+  Ballot ballot1(ballotCandidates);
+
+
+
+///////////////////////////////////////////////////////////////////////////
+// TESTING FOR INCREMENT 
+//////////////////////////////////////////////////////////////////////////
+
+if("Jane"==candidates[ballot1.getCurrBallotIndex()].getName()){
+  cout << "True" << endl;
 }
+  else{
+    cout << "False, candidate initialization went wrong." << endl;
+  }
+
+ballot1.incrCurrent();
+
+if("Dave"==candidates[ballot1.getCurrBallotIndex()].getName()){
+  cout << "True" << endl;
+}
+  else{
+    cout << "False, Incr Ballot is broken." << endl;
+  }
+
+
+///////////////////////////////////////////////////////////////////////////
+// TESTING FOR GET CURR BALLOT
+//////////////////////////////////////////////////////////////////////////
+
+if(1 ==ballot1.getCurrBallot()){
+  cout << "True" << endl;
+}
+else{
+    cout << "False, GetCurrBallot is broken." << endl;
+}
+
+///////////////////////////////////////////////////////////////////////////
+// TESTING FOR GETCANDIDATES
+//////////////////////////////////////////////////////////////////////////
+if(vector<int>{2,3,1} == ballot1.getCandidates()){
+  cout << "True" << endl;
+}
+else{
+    cout << "False, GetCandidates is broken." << endl;
+}
+
+
+
+}//end main
+
+
+
