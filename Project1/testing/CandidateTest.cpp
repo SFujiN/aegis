5a7263c429e4f343492ddb4be61de0ab76da7b1d
Michael <ly000017@umn.edu>
2021-03-25 15:58:02 -0500

    Adding Candidate Test

--- Project1/testing/CandidateTest.cpp
+++ Project1/testing/CandidateTest.cpp
@@ -1,72 +1,123 @@
+#include "../src/Candidate.h"
+#include "../src/Ballot.h"
+#include "gtest/gtest.h"
 #include <vector>
 
-#include "../src/Ballot.cpp"
-#include "../src/Candidate.cpp"
-#include "../src/OPL.cpp"
-#include "../src/Party.cpp"
-#include "../src/VotingSystem.cpp"
 
-using namespace std;
-    Candidate Bob = Candidate('I', "Bob");
+class CandidateTests : public ::testing::Test {
+public:
+    void SetUp() {
+        /*
+        Bob = Candidate('I',"Bob");
+        Jane = Candidate('D',"Jane");
+        David = Candidate('L',"Dave");
+
+       
+    }
+
+protected:
+    //Candidate default;
+    Candidate Bob;
+    Candidate Jane;
+    Candidate David;
+    */
+}
+};
+
+TEST_F(CandidateTests, getNameTest) {
+    Candidate Bob = Candidate('I',"Bob");
+    Candidate Jane = Candidate('D',"Jane");
+    Candidate David = Candidate('L',"Dave");
+    EXPECT_EQ(Bob.getName(),"Bob") << "GetParty has errors";
+    EXPECT_EQ(Jane.getName(),"Jane") << "GetParty has errors";
+    EXPECT_EQ(David.getName(),"Dave") << "GetParty has errors";
+   
+}
+
+TEST_F(CandidateTests, getPartyTest) {
+    Candidate Bob = Candidate('I',"Bob");
+    Candidate Jane = Candidate('2',"Jane");
+    Candidate David = Candidate('*',"Dave");
+    Candidate Space = Candidate(' ',"Space");
+    EXPECT_EQ(Bob.getParty(),'I') << "GetParty has errors";
+    EXPECT_EQ(Jane.getParty(),'2') << "GetParty has errors";
+    EXPECT_EQ(David.getParty(),'*') << "GetParty has errors";
+    EXPECT_EQ(Space.getParty(),' ') << "GetParty has errors";
+   
+}
+
+TEST_F (CandidateTests, getNumBallotsTest) { 
+    Candidate Bob = Candidate('I',"Bob");
+    Candidate Jane = Candidate('D',"Jane");
+    Candidate David = Candidate('L',"Dave");
+    Ballot ballot1({1,2,3});
+    David.addBallot(ballot1);
+    for(int i = 0; i < 100000; i++){
+        Jane.addBallot(ballot1);
+    }
+    EXPECT_EQ(Bob.getNumBallots(),0) << "GetBallots has errors";
+    EXPECT_EQ(Jane.getNumBallots(),100000) << "GetBallots has errors";
+    EXPECT_EQ(David.getNumBallots(),1) << "GetBallots has errors";
+}
+
+TEST_F (CandidateTests, getInitBallotsTest) { 
+    Candidate Bob = Candidate('I',"Bob");
+    Candidate Jane = Candidate('D',"Jane");
+    Candidate David = Candidate('L',"Dave");
+    Bob.setInitBallots(100);
+    Jane.setInitBallots(-10);
+    David.setInitBallots(0);
+    EXPECT_EQ(Bob.getInitBallots(),100) << "GetBallots has errors";
+    EXPECT_EQ(Jane.getInitBallots(),-10) << "GetBallots has errors";
+    EXPECT_EQ(David.getInitBallots(),0) << "GetBallots has errors";
+}
+
+TEST_F (CandidateTests, getNumBallotsTest) { 
+    Candidate Bob = Candidate('I',"Bob");
+    Candidate Jane = Candidate('D',"Jane");
+    Candidate David = Candidate('L',"Dave");
+    std::vector<Candidate> candidates;
+    std::vector<int> ballotCandidates;
+    std::vector<int> ballotCandidates2;
+    std::vector<int> ballotCandidates3;
+    
+    candidates.push_back(Bob);
+    candidates.push_back(Jane);
+    candidates.push_back(David);
+
+    ballotCandidates.push_back(2);
+    ballotCandidates.push_back(3);
+    ballotCandidates.push_back(1);
+
+    ballotCandidates2.push_back(1);
+    ballotCandidates2.push_back(2);
+    ballotCandidates2.push_back(3);
+
+    ballotCandidates3.push_back(3);
+    ballotCandidates3.push_back(2);
+    ballotCandidates3.push_back(1);
+
+    Ballot ballot1(ballotCandidates);
+    Ballot ballot2(ballotCandidates2);
+    Ballot ballot3(ballotCandidates3);
+
+    Bob.addBallot(ballot1);
+    Bob.addBallot(ballot2);
+    Bob.addBallot(ballot3);
+
+    std::vector<Ballot> ballotVec;
+    ballotVec.push_back(ballot1);
+    ballotVec.push_back(ballot2);
+    ballotVec.push_back(ballot3);
+
+    EXPECT_EQ(Bob.getBallots()[0], ballotVec[0]) << "GetBallots has errors";
+    EXPECT_EQ(Bob.getBallots()[1], ballotVec[1]) << "GetBallots has errors";
+    EXPECT_EQ(Bob.getBallots()[2], ballotVec[2]) << "GetBallots has errors";
     
     
-    
-
-
-int main(int argc, char *argv[]) {
-std::vector<int> ballot;
-ballot.push_back(1);
-///////////////////////////////////////////////////////////////////////////
-// TESTING FOR getName
-//////////////////////////////////////////////////////////////////////////
-
-if(Bob.getName() == "Bob"){
-  cout << "True" << endl;
 }
-  else{
-    cout << "False, getName went wrong." << endl;
-  }
-
-///////////////////////////////////////////////////////////////////////////
-// TESTING FOR getParty
-//////////////////////////////////////////////////////////////////////////
-
-if(Bob.getParty() == 'I'){
-  cout << "True" << endl;
-}
-  else{
-    cout << "False, getParty went wrong." << endl;
-  }
-
-///////////////////////////////////////////////////////////////////////////
-// TESTING BALLOT USAGE
-//////////////////////////////////////////////////////////////////////////
-Bob.addBallot(ballot);
-
-if(Bob.getNumBallots() == 1){
-  cout << "True" << endl;
-}
-  else{
-    cout << "False, getNumBallots or addBallot went wrong." << endl;
-  }
-
-if(Bob.getBallots()[0].getCurrBallot() == ballot[1]){
-  cout << "True" << endl;
-}
-  else{
-    cout << "False, getCurrBallot went wrong." << endl;
-  }
-
-Bob.setInitBallots(1);
-if(Bob.getInitBallots() == 1){
-  cout << "True" << endl;
-}
-  else{
-    cout << "False, getInitBallots went wrong." << endl;
-  }
 
 
-}//end main
 
 
 
