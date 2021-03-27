cade74c53a99f7abb0b0ea18e0666884e416e470
vang2959 <vang2959@umn.edu>
2021-03-27 15:05:50 -0500

    adding comments to tests

--- Project1/testing/BallotUnitTest.cpp
+++ Project1/testing/BallotUnitTest.cpp
@@ -11,31 +11,39 @@ class BallotTest : public ::testing::Test {
 };
 
 TEST_F(BallotTest, IncrementCurrent) {
-  ASSERT_EQ(ballot->getCurrBallot(), 0);
-  ASSERT_EQ(ballot->getCurrBallotIndex(), 0);
+  ASSERT_EQ(ballot->getCurrBallot(), 0)
+      << "IncrementCurrent: failed to get the correct current ballot";
+  ASSERT_EQ(ballot->getCurrBallotIndex(), 0)
+      << "IncrementCurrent: failed to get the correct current ballot index";
   ballot->incrCurrent();
-  ASSERT_EQ(ballot->getCurrBallot(), 1);
-  ASSERT_EQ(ballot->getCurrBallotIndex(), 1);
+  ASSERT_EQ(ballot->getCurrBallot(), 1)
+      << "IncrementCurrent: failed to get the correct current ballot";
+  ASSERT_EQ(ballot->getCurrBallotIndex(), 1)
+      << "IncrementCurrent: failed to get the correct current ballot index";
 }
 
 TEST_F(BallotTest, GetCandidates) {
-  ASSERT_EQ(ballot->getCandidates(), (std::vector<int>{1, 2, 3}));
+  ASSERT_EQ(ballot->getCandidates(), (std::vector<int>{1, 2, 3}))
+      << "GetCandidates: failed to get the correct candidate vector";
 }
 
-TEST_F(BallotTest, GetCurrentBallot) { ASSERT_EQ(ballot->getCurrBallot(), 0); }
+TEST_F(BallotTest, GetCurrentBallot) {
+  ASSERT_EQ(ballot->getCurrBallot(), 0)
+      << "GetCurrentBallot: failed to get the correct current ballot";
+}
 
 TEST_F(BallotTest, PrintBallot) {
   std::ostringstream output;
   ballot->printBallot(output);
-  ASSERT_EQ(output.str(),
-            "candidate[0]: 1\ncandidate[1]: 2\ncandidate[2]: 3\n");
+  ASSERT_EQ(output.str(), "candidate[0]: 1\ncandidate[1]: 2\ncandidate[2]: 3\n")
+      << "PrintBallot: failed to print out the correct ballot";
 }
 
 TEST_F(BallotTest, PrintEmptyBallot) {
   Ballot *emptyBallot = new Ballot(std::vector<int>{});
   std::ostringstream output;
   emptyBallot->printBallot(output);
-  ASSERT_EQ(output.str(), "");
+  ASSERT_EQ(output.str(), "")<<"PrintEmptyBallot: should've printed an empty ballot";
 }
 
 int main(int argc, char **argv) {
