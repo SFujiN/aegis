834df39ef9c4339a8f6fd21f6c58063d70947a4d
Michael <ly000017@umn.edu>
2021-04-27 15:55:16 -0500

    Adding documentation to the PO class and fixing documentation from the OPL class.

--- Project2/src/PO.h
+++ Project2/src/PO.h
@@ -7,8 +7,25 @@
 class PO : public VotingSystem {
   public:
   PO(int candidates, int ballots);
-
+  /**
+   * @brief Runs the PO based election.
+   *
+   * Runs all of the methods needed to run a PO election.
+   *
+   * @return void.
+   */
   void runElection();
+
+
+  /**
+   * @brief Prints the results of the election.
+   *
+   * Displays all the information an election official would need in an
+   * election. Such information includes: Number of Ballots, Winners, Losers
+   * and Election Type
+   *
+   * @return void.
+   */
   void displayResults();
 };
 
